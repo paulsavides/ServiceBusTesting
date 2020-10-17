8b09940c909b3943022eaeacc474af6da093ee7e
paulsavides <paulsavides@outlook.com>
2020-10-17 11:20:45 -0500

    Initial commit

---
+++ ReproProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ReproProject
+{
+  public static class Program
+  {
+    public static void Main()
+    {
+      Console.WriteLine("Press any key to exit...");
+      Console.ReadKey();
+    }
+  }
+}
