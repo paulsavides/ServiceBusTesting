8d797f45410c06d61f59ab032e2384667de3a177
paulsavides <paulsavides@outlook.com>
2020-10-17 11:45:50 -0500

    Add initial setup for management client

---
+++ ReproProject/AzureServiceBusWrapper.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.Azure.ServiceBus.Management;
+using Microsoft.Azure.ServiceBus.Primitives;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReproProject
+{
+  public class AzureServiceBusConfiguration
+  {
+    public string Endpoint { get; set; }
+    public string QueueName { get; set; }
+    public string TopicName { get; set; }
+    public int MaxConcurrentCalls { get; set; }
+    public string SharedAccessSignature { get; set; }
+    public string KeyName { get; set; }
+  }
+
+  public class AzureServiceBusWrapper
+  {
+    private AzureServiceBusWrapper() { }
+    public static async Task<AzureServiceBusWrapper> InitializeAsync(AzureServiceBusConfiguration configuration, CancellationToken cancellationToken = default)
+    {
+      var wrapper = new AzureServiceBusWrapper();
+      await wrapper.InitializeInternalAsync(configuration, cancellationToken);
+      return wrapper;
+    }
+
+    private AzureServiceBusConfiguration _config;
+    private ManagementClient _managementClient;
+
+    private async Task InitializeInternalAsync(AzureServiceBusConfiguration configuration, CancellationToken cancellationToken)
+    {
+      cancellationToken.ThrowIfCancellationRequested();
+      _config = configuration;
+
+      var tokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(_config.KeyName, _config.SharedAccessSignature, TimeSpan.FromDays(1));
+      _managementClient = new ManagementClient(_config.Endpoint, tokenProvider);
+
+      if (!await _managementClient.QueueExistsAsync(_config.QueueName, cancellationToken))
+      {
+        await _managementClient.CreateQueueAsync(_config.QueueName, cancellationToken);
+      }
+
+      if (!await _managementClient.TopicExistsAsync(_config.TopicName, cancellationToken))
+      {
+        await _managementClient.CreateTopicAsync(_config.TopicName, cancellationToken);
+      }
+
+      if (!await _managementClient.SubscriptionExistsAsync(_config.TopicName, _config.TopicName, cancellationToken))
+      {
+        await _managementClient.CreateSubscriptionAsync(new SubscriptionDescription(_config.TopicName, _config.TopicName)
+        {
+          ForwardTo = _config.QueueName
+        }, cancellationToken);
+      }
+
+      //var connection = new ServiceBusConnection(_config.Endpoint, TransportType.Amqp, RetryPolicy.Default)
+      //{
+      //  TokenProvider = tokenProvider
+      //};
+    }
+
+    public async Task ShutdownAsync(CancellationToken cancellationToken = default)
+    {
+      cancellationToken.ThrowIfCancellationRequested();
+
+      await _managementClient.CloseAsync();
+    }
+  }
+}
--- ReproProject/Program.cs
+++ ReproProject/Program.cs
@@ -1,13 +1,28 @@
 ﻿using System;
+using System.Threading.Tasks;
 
 namespace ReproProject
 {
   public static class Program
   {
-    public static void Main()
+    public static async Task Main()
     {
-      Console.WriteLine("Press any key to exit...");
+      var config = new AzureServiceBusConfiguration
+      {
+        QueueName = "repro.project.queue",
+        TopicName = "repro.project.topic",
+        MaxConcurrentCalls = 10,
+        Endpoint = "sb://endpoint.servicebus.windows.net",
+        KeyName = "RootManageSharedAccessKey",
+        SharedAccessSignature = "setme"
+      };
+
+      var wrapper = await AzureServiceBusWrapper.InitializeAsync(config);
+
+      Console.WriteLine("Press any key to shut down and exit...");
       Console.ReadKey();
+
+      await wrapper.ShutdownAsync();
     }
   }
 }
