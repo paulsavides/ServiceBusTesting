fb722672becb81e1f48e445fb398f515aa753750
paulsavides <paulsavides@outlook.com>
2020-11-20 10:09:48 -0600

    Add project that reproduces using amqp library more directly

---
+++ ReproAmqp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ReproAmqp
+{
+  public static class Program
+  {
+    public static async Task Main()
+    {
+      var config = new Config
+      {
+        AddDelayAfterOpen = true,
+        KeyName = "RootManageSharedAccessKey",
+        Endpoint = "sb://setme.servicebus.windows.net",
+        SharedAccessSignature = "setme"
+      };
+
+      Console.WriteLine("Press any key to stop the test...");
+      Console.WriteLine();
+
+      var runner = new TestRunner(config);
+
+      var cts = new CancellationTokenSource();
+      var running = runner.RunAsync(cts.Token);
+
+      Console.ReadKey();
+      Console.WriteLine();
+
+      cts.Cancel();
+      await running;
+    }
+  }
+}
---
+++ ReproAmqp/TestRunner.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.Amqp;
+using Microsoft.Azure.ServiceBus;
+using Microsoft.Azure.ServiceBus.Primitives;
+
+namespace ReproAmqp
+{
+  public class Config
+  {
+    public bool AddDelayAfterOpen { get; set; }
+    public string Endpoint { get; set; }
+    public string KeyName { get; set; }
+    public string SharedAccessSignature { get; set; }
+  }
+
+  public class TestRunner
+  {
+    private readonly Random _rand = new Random();
+    private readonly TimeSpan _defaultTimeSpan = TimeSpan.FromSeconds(60);
+
+    private readonly Config _config;
+
+    private int GotOpenConnectionCounter = 0;
+    private int GotClosedConnectionCounter = 0;
+    private int UnsuccessfulManagerCloseCounter = 0;
+
+    public TestRunner(Config config)
+    {
+      _config = config;
+    }
+
+    public async Task RunAsync(CancellationToken token)
+    {
+      while (!token.IsCancellationRequested)
+      {
+        var connectionManager = SetupConnectionManager();
+
+        var creating = connectionManager.GetOrCreateAsync(_defaultTimeSpan);
+        
+        // this is really only added to illustrate that the window for this error is decently large
+        if (_config.AddDelayAfterOpen)
+        {
+          await Task.Delay(_rand.Next(100, 501));
+        }
+
+        await connectionManager.CloseAsync();
+
+        try
+        {
+          var connection = await creating;
+
+          bool managerClosed = false;
+          try
+          {
+            await connectionManager.GetOrCreateAsync(_defaultTimeSpan);
+          }
+          catch (ObjectDisposedException)
+          {
+            managerClosed = true;
+          }
+
+          if (!managerClosed)
+          {
+            ++UnsuccessfulManagerCloseCounter;
+          }
+          else if (!connection.IsClosing())
+          {
+            Console.WriteLine("Got open connection from closed connectionManager!");
+            ++GotOpenConnectionCounter;
+            await connection.CloseAsync(_defaultTimeSpan);
+          }
+          else
+          {
+            ++GotClosedConnectionCounter;
+          }
+        }
+        catch (Exception ex)
+        {
+          Console.WriteLine(ex);
+        }
+      }
+
+      PrintStats();
+    }
+
+    private FaultTolerantAmqpObject<AmqpConnection> SetupConnectionManager()
+    {
+      var tokenProvider = TokenProvider.CreateSharedAccessSignatureTokenProvider(_config.KeyName, _config.SharedAccessSignature, TimeSpan.FromDays(1));
+
+      var connection = new ServiceBusConnection(_config.Endpoint, TransportType.Amqp, RetryPolicy.Default)
+      {
+        TokenProvider = tokenProvider
+      };
+
+      var prop = connection.GetType().GetProperty("ConnectionManager", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Instance);
+      return prop.GetValue(connection) as FaultTolerantAmqpObject<AmqpConnection>;
+    }
+
+    private void PrintStats()
+    {
+      Console.WriteLine($"Overall stats: GotClosedConnection={GotClosedConnectionCounter} GotOpenConnection={GotOpenConnectionCounter} " +
+        $"UnsuccessfulManagerCloseCounter={UnsuccessfulManagerCloseCounter}");
+    }
+  }
+}
